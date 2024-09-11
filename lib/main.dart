030c8e9436fce1596343f95b87116750741764db
sheikh-Tijan-hydara <hydarasheikhtijan30@gmail.com>
2024-09-11 16:30:47 +0000

    added the preserve and remove for the splash

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:my_first_flutter_app/components/app_bar.dart';
 import 'package:my_first_flutter_app/components/main_container.dart';
 import 'package:my_first_flutter_app/components/bottom_navigation.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      primarySwatch: Colors.deepPurple,
-    ),
-    home: const Scaffold(
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
+  FlutterNativeSplash.remove();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const Scaffold(
         appBar: PreferredSize(
-            preferredSize: Size.fromHeight(kToolbarHeight),
-            child: AppBarContainer()),
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: AppBarContainer(),
+        ),
         bottomNavigationBar: BottomNavigation(),
-        body: Center(child: MainContainer())),
-  ));
+        body: Center(child: MainContainer()),
+      ),
+    );
+  }
 }
